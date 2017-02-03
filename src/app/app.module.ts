1a1c257ff404d416f68e5077d883888ca2736120
Lukas Bargoin <bargoin.lukas34@gmail.com>
2017-02-03 14:09:08 +0100

    Initialisation des clasess métiers et des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { SprintComponent } from './sprint/sprint.component';
+import { ReleaseComponent } from './release/release.component';
+import { StoryComponent } from './story/story.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    SprintComponent,
+    ReleaseComponent,
+    StoryComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
